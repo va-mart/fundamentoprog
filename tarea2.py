0fcfca3948c6fdbbc85803b4325c2a0e2b1d474e
va-mart <va.martinoli@duocuc.cl>
2025-04-09 09:31:46 -0400

    Add files via upload

---
+++ tarea2.py
@@ -0,0 +1,27 @@
+#Definir
+V01 = int(input('Ingresa un número entero: '))
+V02 = float(input('Ingresa un número decimal: '))
+
+#Operación
+R1 = V01 + V02
+
+#Salida
+print (f'El resultado es: {R1}')
+
+V03 = int(input('Ingresa un número entero: '))
+V04 = float (input('Ingresa un número decimal: '))
+
+R2 = V03 - V04
+print (f'El resultado es: {R2}')
+
+V05= int(input('Ingresa un número entero: '))
+V06= float(input('Ingresa un número decimal: '))
+
+R3 = V05 * V06
+print(f'El resultado es: {R3}')
+
+V07 = int(input('Ingresa un número entero: '))
+V08 = float(input('Ingresa un número decimal: '))
+
+R4 = V07 / V08
+print(f'El resultado es: {R4}')
