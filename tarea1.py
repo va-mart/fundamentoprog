013cfea0f5ec2e13d3891d39b969e18bded74060
va-mart <va.martinoli@duocuc.cl>
2025-04-09 09:30:34 -0400

    Add files via upload

---
+++ tarea1.py
@@ -0,0 +1,9 @@
+
+#Variable1 = 12
+#Variable2= 12,3
+#Variabletext = "Hola, mundo"
+#Variablelogic = True
+
+Nombre = input ('Escribe tu nombre completo: ')
+Edad = input ('Ingresa tu edad: ')
+print('Bienvenid@ ' + Nombre)
