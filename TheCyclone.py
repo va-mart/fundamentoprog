ba430fbd40d8bb4c05ec23ecd01db561140c74e4
va-mart <va.martinoli@duocuc.cl>
2025-04-16 21:47:10 -0400

    Add files via upload

---
+++ TheCyclone.py
@@ -0,0 +1,19 @@
+#height at least 137 cm, cost 10 credis
+#ask users their height and how many credits do they have
+#height and credits enough -> Enojy the ride
+#enough credits but short -> not tall enough to ride
+# tall enough but not credits -> you don't have enought credits
+# have not met either requirement
+
+print('Welcome to The Cyclone, Coney Island.\n', 'We need to you to answer a couple questions before proceeding.')
+height = int(input('What is your height? Please write it in cm: '))
+credits = int(input('How many credits do you have?: '))
+
+if height >= 137 and credits >= 10:
+  print('Enjoy the ride!')
+elif height < 137 and credits >= 10:
+  print("You're not tall enough to ride.")
+elif height >= 137 and credits < 10:
+  print("You don't have enough credits.")
+else:
+  print("You don't meet either requirement") 
